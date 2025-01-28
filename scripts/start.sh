274962932ed85102fba25581c75506768a5e2280
Kipchumba Bett <kipchubett@gmail.com>
2025-01-28 15:13:42 +0300

    OZ-753: Ozone Analytics run with Ozone FOSS with SSO enabled (#39)

--- scripts/start.sh
+++ scripts/start.sh
@@ -11,10 +11,46 @@ setDockerHost
 # Export the paths variables to point to distro artifacts
 exportEnvs
 
-setTraefikIP
+# Export IP address of the host machine
+if [ "$ENABLE_OAUTH" == "true" ]; then
+  exportHostIP
+fi
 
-setTraefikHostnames
+# Set the Traefik host names
+if [ "$TRAEFIK" == "true" ]; then
+    echo "$INFO \$TRAEFIK=true, setting Traefik hostnames..."
+    setTraefikIP
+    setTraefikHostnames
+else
+    echo "$INFO \$TRAEFIK!=true, setting Nginx hostnames..."
+    setNginxHostnames
+fi
 
 echo "$CONNECT_ODOO_DB_NAME"
 
-docker compose -p ozone-analytics -f ../docker/docker-compose-db.yaml -f ../docker/docker-compose-migration.yaml -f ../docker/docker-compose-streaming-common.yaml -f ../docker/docker-compose-kowl.yaml  -f ../docker/docker-compose-superset.yaml -f ../docker/docker-compose-superset-ports.yaml up -d
+# Run Ozone Analytics Services
+#dockerComposeCommand="docker compose -p ozone-analytics -f ../docker/docker-compose-db.yaml -f ../docker/docker-compose-superset.yaml up -d"
+dockerComposeCommand="docker compose -p ozone-analytics -f ../docker/docker-compose-db.yaml -f ../docker/docker-compose-migration.yaml -f ../docker/docker-compose-streaming-common.yaml -f ../docker/docker-compose-kowl.yaml  -f ../docker/docker-compose-superset.yaml up -d"
+echo "$INFO Running Ozone Analytics Services..."
+echo "$dockerComposeCommand"
+$dockerComposeCommand
+
+# Run Nginx proxy
+if [ "$TRAEFIK" == "true" ]; then
+    echo "$INFO \$TRAEFIK=true, skip running Nginx Proxy..."
+    echo "$INFO Assuming that Traefik is running on the host machine..."
+  else
+    echo "$INFO \$TRAEFIK!=true, running Nginx Proxy..."
+    docker compose -p ozone-analytics -f ../docker/proxy/docker-compose-nginx.yaml up -d
+    echo "$INFO Nginx is running!"
+fi
+
+# Wait for the services to start
+echo "$INFO Waiting for the services to start..."
+sleep 10 # Wait for 10 seconds
+
+# Display Access URLs
+echo "$INFO Ozone Analytics Services are running!"
+echo "$INFO Access URLs:"
+echo "$INFO Superset: $SCHEME://$SUPERSET_HOSTNAME ($([ "$ENABLE_OAUTH" != "true" ] && echo "admin" || echo "jdoe") / password)"
+echo "$INFO Keycloak: $SCHEME://$KEYCLOAK_HOSTNAME (admin / password)"
